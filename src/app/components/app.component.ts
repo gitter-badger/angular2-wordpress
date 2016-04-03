a2d10122c878ebbf09f2cdc9ee58498e3c31b2e3
Thanh Nguyen <Sun.SmartBiz@gmail.com>
2016-04-03 21:00:39 +0700

    search.component

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -4,17 +4,23 @@ import {NavbarComponent} from './navbar.component';
 import {ToolbarComponent} from './toolbar.component';
 import {HomeComponent} from '../../home/components/home.component';
 import {AboutComponent} from '../../about/components/about.component';
+
+import {SearchComponent} from '../../search/components/search.component';
 import {NameListService} from '../../shared/services/name-list.service';
+import {SearchService} from '../../shared/services/search.service';
+import {EditComponent} from '../../search/components/edit.component';
 
 @Component({
   selector: 'sd-app',
-  viewProviders: [NameListService],
+  viewProviders: [NameListService, SearchService],
   moduleId: module.id,
   templateUrl: './app.component.html',
   directives: [ROUTER_DIRECTIVES, NavbarComponent, ToolbarComponent]
 })
 @RouteConfig([
   { path: '/',      name: 'Home',  component: HomeComponent  },
-  { path: '/about', name: 'About', component: AboutComponent }
+  { path: '/about', name: 'About', component: AboutComponent },
+  { path: '/search', name: 'Search', component: SearchComponent },
+  { path: '/edit/:name', name: 'Edit', component: EditComponent }
 ])
 export class AppComponent {}
